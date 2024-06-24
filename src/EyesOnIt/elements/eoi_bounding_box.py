50bc76d85ab03220fa7d36bd1a6c319fdfdf26ce
“Christian <ctuchez@mosaicatm.com>
2024-06-24 13:36:23 -0400

    changed people_confidence_threshold to person_confidence_threshold

--- src/EyesOnIt/elements/eoi_bounding_box.py
+++ src/EyesOnIt/elements/eoi_bounding_box.py
@@ -6,6 +6,6 @@ class EOIBoundingBox(BaseModel):
     detect_people: bool = False
     detect_vehicles: bool = False
     detect_bags: bool = False
-    people_confidence_threshold: int = 30
+    person_confidence_threshold: int = 30
     vehicle_confidence_threshold: int = 30
     bag_confidence_threshold: int = 30
